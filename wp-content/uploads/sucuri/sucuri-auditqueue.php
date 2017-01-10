8bda4be9a69d4cb6c65fb060addcf1d48261059b
Maulik <mk@narola.email>
2017-01-10 17:26:50 +0530

    Woocommerce Booking plugin integration

---
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=auditqueue;
+// created_on=1484048189;
+// updated_on=1484048189;
+exit(0);
+?>
---
+++ wp-content/uploads/sucuri/sucuri-sitecheck.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=sitecheck;
+// created_on=1484047095;
+// updated_on=1484047095;
+exit(0);
+?>
